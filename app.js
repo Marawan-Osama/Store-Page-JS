b0dafd1ab20d4fdfb15e3a3ddde56560c69950b8
Marawan-Osama <marawanegy@gmail.com>
2023-09-03 17:37:53 +0300

    Imported PORT from .env

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./db/connect');
+const products = require('./routes/products');
 const not_found = require('./middleware/not-found');
 const error_handler = require('./middleware/error-handler');
 require('dotenv').config();
 
 app.use(express.json());
-app.use('/api/v1', require('./routes/products'));
+app.use('/api/v1', products);
 app.use(not_found);
 app.use(error_handler);
 
 
-
+port = process.env.PORT || 3000;
 
 const start = async ()=>{
     try{
         await connectDB(process.env.MONGO_URI)
-        await app.listen(3000);
-        console.log('server started at port 3000');
+        await app.listen(port);
+        console.log(`server started at port ${port}`);
     }catch(err){
         console.log(err);
     }
